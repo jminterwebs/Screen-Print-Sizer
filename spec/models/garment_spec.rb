eee648b844b494dbae9a50a0a1351a88f1b5a4e6
Unknown <jmverdone@gmail.com>
2018-01-20 21:35:23 -0500

    validation tests

--- spec/models/garment_spec.rb
+++ spec/models/garment_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
-describe Garment do
+describe Garment, type: :model do
 
-it 'has a style code and name' do
-  expect {Garment.create(:style_code => '207', :name => 'Classic Tee Shirt')}.to_not raise_error
+  subject {
+      described_class.new(style_code: "207", name: "Classic Tee Shirt")
+    }
 
+it 'is valid with vlid attirubutes' do
+  expect(subject).to be_valid
 end
 
+it 'is not valid without a name' do
+  subject.name = nil
+  expect(subject).to_not be_valid
+end
+
+it 'should not be valid without a style_code' do
+  subject.style_code = nil
+  expect(subject).to_not be_valid
+end
 
 end
