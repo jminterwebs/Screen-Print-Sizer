8519efad016aad76fb620ffecb63d78e1964572d
Unknown <jmverdone@gmail.com>
2018-01-21 00:03:24 -0500

    tests for location_code and name

--- spec/models/locations_spec.rb
+++ spec/models/locations_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 describe Location, type: :model do
   subject { described_class.new(location_code: "FC", name: "Full Chest")}
 
-  it "should be valid with valid attirubutes" do
+  it 'should be valid with valid attirubute' do
     expect(subject).to be_valid
+  end
+
+  it 'should not be valid without a location_code' do
+    subject.location_code = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
 end
