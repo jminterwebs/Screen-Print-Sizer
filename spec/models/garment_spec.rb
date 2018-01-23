bff62e6e8ee7f81fd1f20aa3f0cfe638f23e9294
Unknown <jmverdone@gmail.com>
2018-01-22 21:59:41 -0500

    relationship testing with location

--- spec/models/garment_spec.rb
+++ spec/models/garment_spec.rb
@@ -2,22 +2,30 @@ require 'rails_helper'
 
 describe Garment, type: :model do
 
-  subject {
+    subject {
       described_class.new(style_code: "207", name: "Classic Tee Shirt")
     }
 
-it 'is valid with valid attirubutes' do
-  expect(subject).to be_valid
+
+describe 'Assications' do
+  it {should have_many(:locations).through(:garmentsLocation)}
 end
 
-it 'is not valid without a name' do
-  subject.name = nil
-  expect(subject).to_not be_valid
+describe "Validations" do
+  it 'should be valid with valid attirubutes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid without a style_code' do
+    subject.style_code = nil
+    expect(subject).to_not be_valid
+  end
 end
 
-it 'should not be valid without a style_code' do
-  subject.style_code = nil
-  expect(subject).to_not be_valid
-end
 
 end
