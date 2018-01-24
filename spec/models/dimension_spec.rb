0b4269e8c93b4a3d86e7d94efcbbe389d2021c6e
Unknown <jmverdone@gmail.com>
2018-01-23 20:48:51 -0500

    tests for location/dimension join

--- spec/models/dimension_spec.rb
+++ spec/models/dimension_spec.rb
@@ -3,18 +3,26 @@ require 'rails_helper'
 describe Dimension, type: :model do
   subject { described_class.new(width: 12, height: 12)}
 
-  it "should have valid attirubutes" do
-    expect(subject).to be_valid
+  describe "Assications" do
+      it {should have_many(:locations).through(:locationsDimension)}
   end
 
-  it 'should not be valid without a height' do
-    subject.height = nil
-    expect(subject).to_not be_valid
-  end
 
-  it 'should not be valid without a width' do
-    subject.width = nil
-    expect(subject).to_not be_valid
-  end
+  describe "Validations" do
 
+    it "should have valid attirubutes" do
+      expect(subject).to be_valid
+    end
+
+    it 'should not be valid without a height' do
+      subject.height = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not be valid without a width' do
+      subject.width = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
 end
--- spec/models/locations_spec.rb
+++ spec/models/locations_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 describe Location, type: :model do
   subject { described_class.new(location_code: "FC", name: "Full Chest")}
 
-  it 'should be valid with valid attirubute' do
-    expect(subject).to be_valid
+
+  describe "Assications" do
+    it {should have_many(:garments).through(:garmentsLocation)}
+
+    it {should have_many(:dimensions).through(:locationsDimension)}
   end
 
-  it 'should not be valid without a location_code' do
-    subject.location_code = nil
-    expect(subject).to_not be_valid
-  end
+  describe "Validations" do
+    it 'should be valid with valid attirubute' do
+      expect(subject).to be_valid
+    end
 
-  it 'should not be valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    it 'should not be valid without a location_code' do
+      subject.location_code = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not be valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
   end
 
 end
