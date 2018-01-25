8c3d802f54ae0371784b9a8b7d0a58c731919538
Unknown <jmverdone@gmail.com>
2018-01-24 21:38:15 -0500

    changed to optional because of Rails 5

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ApplicationRecord
   has_many :garmentsLocation
   has_many :garments, through: :garmentsLocation
 
-  belongs_to :dimension
+  belongs_to :dimension, optional: true
 
   validates_presence_of :location_code, :name
 end
