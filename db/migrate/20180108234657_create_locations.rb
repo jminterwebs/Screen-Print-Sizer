c19acf2960a4674dc6866468f47a93666f2e7679
Unknown <jmverdone@gmail.com>
2018-01-24 20:04:06 -0500

    changed assication

--- db/migrate/20180108234657_create_locations.rb
+++ db/migrate/20180108234657_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration[5.0]
     create_table :locations do |t|
       t.string :location_code
       t.string :name
+      t.integer :dimension_id
       t.timestamps
     end
   end
