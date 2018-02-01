5f12a0e777c4de5d9de8ae1e7cad76c02e4241f9
Unknown <jmverdone@gmail.com>
2018-01-31 22:05:46 -0500

    show method and before action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,20 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: [:show]
 
   def index
     @locations = Location.all
 
     json_response(@locations)
+  end
+
+  def show
+    json_response(@location)
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
   end
 
 end
