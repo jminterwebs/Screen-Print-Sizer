8549da0a17d52b5299f210ef3fbb0a81428c6d53
Unknown <jmverdone@gmail.com>
2018-01-31 21:56:54 -0500

    location factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,11 @@ FactoryBot.define do
 
   factory :garment do
     name "Classic Tee Shirt"
-    style_code 207
+    style_code "207"
   end
 
+  factory :location do
+    name "Full Chest"
+    location_code "FC"
+  end
 end
