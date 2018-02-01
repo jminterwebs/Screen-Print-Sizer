dbb6abb99371e0716d3d002f5de9e4194bedb4b0
Unknown <jmverdone@gmail.com>
2018-01-31 22:20:52 -0500

    testing for one location

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -18,8 +18,48 @@ RSpec.describe "Locations", type: :request do
     it 'returns a 200 status code' do
       expect(response).to have_http_status(200)
     end
-
   end
-  
+
+  #GET tests for /locations/:id
+  describe 'GET /locations/:id' do
+    before {get "/locations/#{location_id}"}
+
+    context "when Record exists" do
+      it "returns one location" do
+        expect(json).to_not be_empty
+        expect(json['id']).to eq(location_id)
+      end
+
+      it 'returns a 200 status code' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when record does not exists' do
+      let(:location_id){100}
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Location/)
+      end
+    end
+  end
+
+  #POST test for /Locations
+  describe "POST /locations" do
+    let(:valid_attributes){{name: "Full Chest", location_code: "FC"}}
+
+    context 'when request is valid' do
+      before {post '/locations', params: valid_attributes}
+
+      it 'creates a location' do
+        expect(json['name']).to eq("Full Chest")
+        expect(json['location_code']).to eq("FC")
+      end
+    end
+  end
 
 end
