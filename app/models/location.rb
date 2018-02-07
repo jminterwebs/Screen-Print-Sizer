29541425f6e31112aa10d1fec5486e7e0a2c1cc6
Unknown <jmverdone@gmail.com>
2018-02-06 23:42:03 -0500

    removed dimension

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,5 @@ class Location < ApplicationRecord
   has_many :garmentsLocation
   has_many :garments, through: :garmentsLocation
 
-  belongs_to :dimension, optional: true
-
   validates_presence_of :location_code, :name
 end
