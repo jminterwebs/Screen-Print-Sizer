0122a8268fb8a773c6e20a094340f418667dd436
Unknown <jmverdone@gmail.com>
2018-02-08 23:21:30 -0500

    nest route index test

---
+++ spec/requests/garmentLocation_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Garment Locations", type: :request do
+
+  let!(:location) {create(:location, :with_garment)}
+  let(:garment_id) {location.garments.first.id}
+  let(:id) {location.id}
+
+  describe "GET /garments/:garment_id/locations" do
+    before { get "/garments/#{garment_id}/locations"}
+
+    context "when Location exists" do
+      it 'resturns a status 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all Garment Locations' do
+        expect(json.size).to eq(1)
+      end
+    end
+
+
+
+  end
+
+end
