910130b23b59d201c6fdd01c73dbca8e954483a3
Unknown <jmverdone@gmail.com>
2018-02-11 16:44:44 -0500

    relationship in factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,10 +4,17 @@ FactoryBot.define do
   factory :garment do
     name "Classic Tee Shirt"
     style_code "207"
+
   end
 
   factory :location do
     name "Full Chest"
     location_code "FC"
+
+    trait :with_garment do
+      after(:create) do |location|
+        location.garments << create(:garment)
+      end
+    end
   end
 end
