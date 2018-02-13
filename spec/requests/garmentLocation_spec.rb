b2be41c59d2287d5639bd0ce280f5a48ec047dc3
Unknown <jmverdone@gmail.com>
2018-02-12 22:16:59 -0500

    get location id test

--- spec/requests/garmentLocation_spec.rb
+++ spec/requests/garmentLocation_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Garment Locations", type: :request do
   let(:garment_id) {location.garments.first.id}
   let(:id) {location.id}
 
+
   #Tests for /garments/:garment_id/locations
   describe "GET /garments/:garment_id/locations" do
     before { get "/garments/#{garment_id}/locations"}
@@ -31,31 +32,45 @@ RSpec.describe "Garment Locations", type: :request do
         expect(response.body).to match(/Couldn't find Garment/)
       end
     end
+  end
 
-    #Tests for #Tests for /garments/:garment_id/locations
-    describe "Get /garments/:garment_id/locations/:id" do
-      before { get "/garments/#{garment_id}/locations/#{id}"}
 
-      context "When Location exists" do
+  #Tests  for /garments/:garment_id/locations
+  describe "Get /garments/:garment_id/locations/:id" do
+    before { get "/garments/#{garment_id}/locations/#{id}"}
 
-        it 'returns a status 200' do
-          expect(response).to have_http_status(200)
-        end
 
-        it 'retruns the location id' do
-          expect(json['id']).to eq(id)
-        end
-
+    context "When Location exists" do
+      it 'returns a status 200' do
+        expect(response).to have_http_status(200)
       end
 
-
+      it 'retruns the location id' do
+        expect(json['id']).to eq(id)
+      end
     end
 
+    context "When Location does not exist" do
+      let(:id) { 0 }
 
+      it "returns  status 404" do
+        expect(response).to have_http_status(404)
+      end
 
+      it "returns a message" do
+        expect(response.body).to match(/Couldn't find Location/)
+      end
+    end
+  end
+
+  #Tests for PUT /garments/:garment_id/locations/:id
+  describe "PUT /garments/:garment_id/locations/:id" do
+    let(:valid_attributes) {{name: "FULL BACK"}}
+    before { put "/garments/#{garment_id}/locations/#{id}"}
 
 
 
   end
+
 
 end
