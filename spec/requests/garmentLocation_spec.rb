bafd9dcb1b09c3f65a985617e90d70b08e0e3928
Unknown <jmverdone@gmail.com>
2018-02-12 21:22:13 -0500

    tests for garment/location nest

--- spec/requests/garmentLocation_spec.rb
+++ spec/requests/garmentLocation_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe "Garment Locations", type: :request do
   let(:garment_id) {location.garments.first.id}
   let(:id) {location.id}
 
+  #Tests for /garments/:garment_id/locations
   describe "GET /garments/:garment_id/locations" do
     before { get "/garments/#{garment_id}/locations"}
 
-    context "when Location exists" do
+    context "when Locations exists" do
       it 'returns a status 200' do
         expect(response).to have_http_status(200)
       end
@@ -29,9 +30,24 @@ RSpec.describe "Garment Locations", type: :request do
       it "resturns a not gound messag" do
         expect(response.body).to match(/Couldn't find Garment/)
       end
+    end
+
+    #Tests for #Tests for /garments/:garment_id/locations
+    describe "Get /garments/:garment_id/locations/:id" do
+      before { get "/garments/#{garment_id}/locations/#{id}"}
+
+      context "When Location exists" do
+
+        it 'returns a status 200' do
+          expect(response).to have_http_status(200)
+        end
+
+      end
 
 
     end
+
+
 
 
 
